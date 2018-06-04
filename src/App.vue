bace6439db945dccbb1de849ca106d456406f29d
jiananWang <a0918666589@gmail.com>
2018-06-05 00:37:35 +0800

    add Post Form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <new-post-form @newPost="newPost"></new-post-form>
     <note-board :notes="notes"></note-board>
   </div>
 </template>
 
 <script>
 import NoteBoard from "./components/NotesBoard";
+import NewPostForm from "./components/NewPostForm";
 
 export default {
   name: "App",
@@ -15,8 +17,18 @@ export default {
       maxNotes: 10
     };
   },
+  methods: {
+    newPost(post) {
+      if (!post) {
+        alert("No content!");
+        return;
+      }
+      this.notes.push(post);
+    }
+  },
   components: {
-    NoteBoard
+    NoteBoard,
+    NewPostForm
   }
 };
 </script>
---
+++ src/components/NewPostForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <form class="col-12 p-0">
+      <div class="col-12 col-md-8 offset-md-2 col-xl-6 offset-md-3 form-group">
+        <label for="new-post">New Post</label>
+        <textarea id="new-post" rows="3" class="form-control" v-model="post"></textarea>
+      </div>
+      <div class="col-4 col-md-3 offset-md-2 offset-md-3 form-group">
+        <button class="btn btn-outline-primary" @click.prevent="newPost">New Post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: ""
+    };
+  },
+  methods: {
+    newPost() {
+      this.$emit("newPost", this.post);
+      this.post = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
